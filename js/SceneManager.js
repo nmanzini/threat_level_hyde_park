21fb97643fe2413d700026b477eb42e3c0f7f015
Nicola <manzini.n@gmail.com>
2018-11-22 01:02:26 -0600

    edge trees

--- js/SceneManager.js
+++ js/SceneManager.js
@@ -1,5 +1,5 @@
 function SceneManager(canvas) {
-
+    
     const clock = new THREE.Clock();
     
     const screenDimensions = {
@@ -11,18 +11,18 @@ function SceneManager(canvas) {
     const renderer = buildRender(screenDimensions);
     const camera = buildCamera(screenDimensions);
     const sceneSubjects = createSceneSubjects(scene,camera);
-
+    
     function buildScene() {
         const scene = new THREE.Scene();
         scene.background = new THREE.Color("#000");
-
+        
         // shows axes
         var axesHelper = new THREE.AxesHelper( 5 );
         scene.add( axesHelper );
-    
+        
         var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
         scene.add( light );
-
+        
         var loader = new THREE.GLTFLoader();
         loader.load(
             // resource URL
@@ -34,7 +34,7 @@ function SceneManager(canvas) {
                 tree01.scale.x = 0.01;
                 tree01.scale.y = 0.01;
                 tree01.scale.z = 0.01;
-
+                
                 // scene.add(tree01)
                 for ( var i = 0; i < 500; i ++ ) {
                     var mesh = tree01.clone ();
@@ -45,7 +45,21 @@ function SceneManager(canvas) {
                     mesh.matrixAutoUpdate = false;
                     scene.add( mesh );
                 }
-        
+                let limit = 200;
+                for ( var i = -1*(limit/2); i < limit/2; i ++ ) {
+                    for(let j=0; j<4; j++)
+                    {
+                        var mesh1 = tree01.clone ();
+                        
+                        mesh1.position.x =  Math.pow(-1,parseInt((j)/2)) * Math.pow(limit/2, (j+1)%2) * Math.pow(i,j%2);
+                        mesh1.position.y = 0;
+                        mesh1.position.z = Math.pow(-1,parseInt((j)/2)) * Math.pow(limit/2, (j)%2) * Math.pow(i,(j+1)%2);
+                        mesh1.updateMatrix();
+                        mesh1.matrixAutoUpdate = false;
+                        scene.add( mesh1 );    
+                    }
+                }
+                
             },
             // called while loading is progressing
             function ( xhr ) {
@@ -55,81 +69,81 @@ function SceneManager(canvas) {
             function ( error ) {
                 console.log( 'An error happened' );
             }
-        );
-        
-        let limit = 300;
-        for ( var i = -1*(limit/2); i < limit/2; i ++ ) {
-            for(let j=0; j<4; j++)
-            {
-                var mesh1 = new THREE.Mesh( geometry, material );
-
-                mesh1.position.x =  Math.pow(-1,parseInt((j)/2)) * Math.pow(limit/2, (j+1)%2) * Math.pow(i,j%2);
-                mesh1.position.y = 0;
-                mesh1.position.z = Math.pow(-1,parseInt((j)/2)) * Math.pow(limit/2, (j)%2) * Math.pow(i,(j+1)%2);
-                mesh1.updateMatrix();
-                mesh1.matrixAutoUpdate = false;
-                scene.add( mesh1 );    
-            }
+            );
+            
+            // let limit = 300;
+            // for ( var i = -1*(limit/2); i < limit/2; i ++ ) {
+            //     for(let j=0; j<4; j++)
+            //     {
+            //         var mesh1 = new THREE.Mesh( geometry, material );
+            
+            //         mesh1.position.x =  Math.pow(-1,parseInt((j)/2)) * Math.pow(limit/2, (j+1)%2) * Math.pow(i,j%2);
+            //         mesh1.position.y = 0;
+            //         mesh1.position.z = Math.pow(-1,parseInt((j)/2)) * Math.pow(limit/2, (j)%2) * Math.pow(i,(j+1)%2);
+            //         mesh1.updateMatrix();
+            //         mesh1.matrixAutoUpdate = false;
+            //         scene.add( mesh1 );    
+            //     }
+            // }
+            
+            return scene;
         }
-
-        return scene;
-    }
-
-    function buildRender({ width, height }) {
-        const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true, alpha: true }); 
-        const DPR = (window.devicePixelRatio) ? window.devicePixelRatio : 1;
-        renderer.setPixelRatio(DPR);
-        renderer.setSize(width, height);
-
-        renderer.gammaInput = true;
-        renderer.gammaOutput = true; 
-
-        return renderer;
-    }
-
-    function buildCamera({ width, height }) {
-        const aspectRatio = width / height;
-        const fieldOfView = 60;
-        const nearPlane = 1;
-        const farPlane = 100; 
-        const camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearPlane, farPlane);
-        camera.position.set(0,25,25);
-        camera.rotation.set(-Math.PI/4,0,0)
-
-        return camera;
-    }
-
-
-    function createSceneSubjects(scene,camera) {
-        const sceneSubjects = [
-            new Car(scene,camera)
-        ];
-
-        return sceneSubjects;
-    }
-
-    this.camera = camera;
-
-
-
-    this.update = () => {
-        const elapsedTime = clock.getElapsedTime();
-
-        for(let i=0; i<sceneSubjects.length; i++)
-        	sceneSubjects[i].update(elapsedTime,camera);
-
-        renderer.render(scene, camera);
-    }
-
-    this.onWindowResize = function() {
-        const { width, height } = canvas;
-
-        screenDimensions.width = width;
-        screenDimensions.height = height;
-
-        camera.aspect = width / height;
-        camera.updateProjectionMatrix();
         
-        renderer.setSize(width, height);
+        function buildRender({ width, height }) {
+            const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true, alpha: true }); 
+            const DPR = (window.devicePixelRatio) ? window.devicePixelRatio : 1;
+            renderer.setPixelRatio(DPR);
+            renderer.setSize(width, height);
+            
+            renderer.gammaInput = true;
+            renderer.gammaOutput = true; 
+            
+            return renderer;
+        }
+        
+        function buildCamera({ width, height }) {
+            const aspectRatio = width / height;
+            const fieldOfView = 60;
+            const nearPlane = 1;
+            const farPlane = 100; 
+            const camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearPlane, farPlane);
+            camera.position.set(0,25,25);
+            camera.rotation.set(-Math.PI/4,0,0)
+            
+            return camera;
+        }
+        
+        
+        function createSceneSubjects(scene,camera) {
+            const sceneSubjects = [
+                new Car(scene,camera)
+            ];
+            
+            return sceneSubjects;
+        }
+        
+        this.camera = camera;
+        
+        
+        
+        this.update = () => {
+            const elapsedTime = clock.getElapsedTime();
+            
+            for(let i=0; i<sceneSubjects.length; i++)
+            sceneSubjects[i].update(elapsedTime,camera);
+            
+            renderer.render(scene, camera);
+        }
+        
+        this.onWindowResize = function() {
+            const { width, height } = canvas;
+            
+            screenDimensions.width = width;
+            screenDimensions.height = height;
+            
+            camera.aspect = width / height;
+            camera.updateProjectionMatrix();
+            
+            renderer.setSize(width, height);
+        }
     }
-}
